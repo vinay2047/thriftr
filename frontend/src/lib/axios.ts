4c29d3f82d2dcd1ea26cc3c6f3926316260c1614
Vinay Mohan <vinaymohanvinod@gmail.com>
2025-09-01 21:56:26 +0530

    fixed axios error

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance= axios.create({
-    baseURL: import.meta.env.NODE_ENV==="production" ? "/api":"http://localhost:3000/api",
+    baseURL:"/api",
     withCredentials: true,
 });
